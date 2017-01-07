cbfa9dc248fb93fa47f17a1c67d5eea0c1e8a0e3
Marc Halbrügge <halbruegge@gmail.com>
2017-01-07 14:56:37 +0100

    Vorlage

---
+++ src/main/java/Bird.java
@@ -0,0 +1,8 @@
+// -*- mode: Java; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+import java.awt.Rectangle;
+
+public class Bird extends ABObject {
+  public Bird(Rectangle mbr, ABType type) {
+    super(mbr, type);
+  }
+}
