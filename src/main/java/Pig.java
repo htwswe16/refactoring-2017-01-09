fb2c157b649627d2f0e0d88af74b55adec06eb28
s0548105 <s0548105@htw-berlin.de>
2017-01-09 12:08:37 +0100

    Hi

---
+++ src/main/java/Pig.java
@@ -0,0 +1,12 @@
+// -*- mode: Java; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+import java.awt.Rectangle;
+
+public class Pig extends ABObject {
+  public Pig(Rectangle mbr, ABType type) {
+    super(mbr, type);
+  }
+  
+    public int getPoints()  {
+    return 1000;
+  }
+}
