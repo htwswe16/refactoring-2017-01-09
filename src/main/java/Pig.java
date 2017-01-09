56600c569c87fa4cc55fa81aab875070d26f78ca
s0551558 <s0551558@htw-berlin.de>
2017-01-09 12:09:46 +0100

    TestPig 1

---
+++ src/main/java/Pig.java
@@ -0,0 +1,11 @@
+// -*- mode: Java; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+import java.awt.Rectangle;
+
+public class Pig extends ABObject {
+  public Pig(Rectangle mbr, ABType type) {
+    super(mbr, type);
+  }
+  public int getPoints() {
+	  return 1000;
+  }
+  }
