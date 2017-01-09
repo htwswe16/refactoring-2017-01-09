3016a74dad8cb26b05bfa101cb51a6e73a7b5d6b
Marc Halbrügge <halbruegge@gmail.com>
2017-01-09 12:09:34 +0100

    Pig

---
+++ src/main/java/Pig.java
@@ -0,0 +1,13 @@
+// -*- mode: Java; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+import java.awt.Rectangle;
+
+public class Pig extends ABObject {
+  public Pig(Rectangle mbr, ABType type) {
+    super(mbr, type);
+  }
+  
+  @Override 
+  public int getPoints() {
+    return 1000;
+  }
+}
